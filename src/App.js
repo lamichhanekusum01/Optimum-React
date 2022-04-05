35d0b964534ad184d6c1d57093c129808e1074e2
Kusum <Kusumlamichhane2015@gmail.com>
2022-04-05 16:59:59 +0545

    Form Added in React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    return (
+        <>
+      <div>
+              <center> <h1> Student Login Form </h1> </center>   
+    <form>  
+        <div class="container">   
+            <label>Username : </label>   
+            <input type="text" placeholder="Enter Username" name="username" required/>  
+            <label>Password : </label>   
+            <input type="password" placeholder="Enter Password" name="password" required/>  
+            <button type="submit">Login</button>   
+            <input type="checkbox" checked="checked"/> Remember me   
+            <button type="button" class="cancelbtn"> Cancel</button>   
+            Forgot <a href=" "> password? </a>   
+        </div>   
+    </form>   
+      </div>
+      </>
+    );
+  }
+  
+  export default App;
